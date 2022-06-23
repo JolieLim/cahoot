54dfe9779951bf62586da60300e35e6dcf24b133
Jeong Wonho <jeongwonho64@gmail.com>
2022-06-23 15:21:37 +0800

    darkmode

--- cahoot/CircularProgressView.swift
+++ cahoot/CircularProgressView.swift
@@ -13,10 +13,12 @@ struct CircularProgressView: View {
     var correc: Int
     var body: some View {
         ZStack {
+            Color.black
             Circle()
                 .stroke(lineWidth: 20)
                 .opacity(0.3)
                 .foregroundColor(.red)
+                .background(.black)
                 .padding()
             Circle()
                 .trim(from: 0, to:progress)
@@ -25,30 +27,31 @@ struct CircularProgressView: View {
                 .rotationEffect(Angle(degrees: 270))
                 .padding()
             Text("You got \(correc)/\(Quiz.count) correct!")
+                .foregroundColor(.white)
         }
             if progress == 0{
                 Text("Aww man go google a link to watch paw patrol free online (pirating ahem)")
-                    .foregroundColor(.black)
+                    .foregroundColor(.white)
                     .padding()
             }
             else if progress == 1/4{
                 Text("Aww man go google a link to watch paw patrol free online (pirating ahem)")
-                    .foregroundColor(.black)
+                    .foregroundColor(.white)
                     .padding()
             }
             else if progress == 0.5{
                 Text("You passed, congratulations - but you still kinda suck :/")
-                    .foregroundColor(.black)
+                    .foregroundColor(.white)
                     .padding()
             }
             else if progress == 3/4{
                 Text("THE FATAL FLAW IN YOUR SEEMINGLY COMPLETE KNOWLEDGE HAS SURFACED GO TO THE WIKI AND PATCH IT UP - YOU WERE SO CLOSE TO PERFECTION SIGH")
-                    .foregroundColor(.black)
+                    .foregroundColor(.white)
                     .padding()
             }
             else{
                 Text("Your childhood has been certified to be amazing, perfect and marvellous")
-                    .foregroundColor(.black)
+                    .foregroundColor(.white)
                     .padding()
 
             }
